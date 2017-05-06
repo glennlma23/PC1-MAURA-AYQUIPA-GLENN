de6f02bb938b507345a28af8cc7b70b1309553cf
glennlma23 <glenn-maura@hotmail.com>
2017-05-06 10:24:14 -0500

    Update index.php

--- index.php
+++ index.php
@@ -21,23 +21,23 @@
 	<table border="1px" align="left">
  			 <?php
 				  $servername = "us-cdbr-iron-east-03.cleardb.net";
-				  $username = "b4463086f96755";
-				  $password = "f20b01d3";
-				  $dbname = "ad_0d961dfda46f40f";
+				  $username = "b74ba3320e82ec";
+				  $password = "2d194843";
+				  $dbname = "ad_bb6ddb2f34daf52";
 				  // Create connection
 				  $conn = new mysqli($servername, $username, $password, $dbname);
 				  // Check connection
 				  if ($conn->connect_error) {
 				      die("Connection failed: " . $conn->connect_error);
 				  }
-				  $sql = "SELECT * from datascientists";
+				  $sql = "SELECT * from servicios";
 				  $result = $conn->query($sql);
 				  if ($result->num_rows > 0) {
 				      // output data of each row
 				      while($row = $result->fetch_assoc()) {?>
 				      <tr>
 				     
-				      <td><?php echo $row['titulods']?><br><?php echo $row['descripcionds']?></td>
+				    <td><?php echo $row['nombre_servicio']?><br><?php echo $row['descripcion_servicio']?></td>
 				   
 				   	  </tr>	
 				      
@@ -51,24 +51,24 @@
 	 		
  	<table border="1px" align="right">	
  			 <?php
-				  $servername = "us-cdbr-iron-east-03.cleardb.net";
-				  $username = "b4463086f96755";
-				  $password = "f20b01d3";
-				  $dbname = "ad_0d961dfda46f40f";
+				   $servername = "us-cdbr-iron-east-03.cleardb.net";
+  				   $username = "b74ba3320e82ec";
+				   $password = "2d194843";
+				   $dbname = "ad_bb6ddb2f34daf52";
 				  // Create connection
 				  $conn = new mysqli($servername, $username, $password, $dbname);
 				  // Check connection
 				  if ($conn->connect_error) {
 				      die("Connection failed: " . $conn->connect_error);
 				  }
-				  $sql = "SELECT * from itprofessionals";
+				  $sql = "SELECT * from servicios";
 				  $result = $conn->query($sql);
 				  if ($result->num_rows > 0) {
 				      // output data of each row
 				      while($row = $result->fetch_assoc()) {?>
 				      <tr>
 				     
-				      <td><?php echo $row['tituloip']?><br><?php echo $row['descripcionip']?></td>
+				      <td><?php echo $row['nombre_servicio']?><br><?php echo $row['descripcion_servicio']?></td>
 				   
 				   	  </tr>	
 				      
